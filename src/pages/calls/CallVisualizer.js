4366930cd28c1a77845976d4fa420460f08a61b4
Jayanth Valluru <Jayanth.Valluru@XEBIAINDIA.LOCAL>
2021-11-12 15:13:37 +0530

    Adding audio visualizer

--- src/pages/calls/CallVisualizer.js
+++ src/pages/calls/CallVisualizer.js
@@ -2,30 +2,15 @@ import React from 'react';
 
 import { Card, Typography } from '@material-ui/core';
 import { useDemoData } from '@mui/x-data-grid-generator';
-// import WavePlayer from 'waveplayer';
 
 import Comments from '../../components/calls/comments/Comments';
 import Moments from '../../components/calls/moments/Moments';
 import Transcription from '../../components/calls/transcription/Transcription';
-import Visualizer from '../../components/calls/visualizer/Visualizer';
+import WavesurferAudioVisualizer from '../../components/calls/wavesurfer-visualizer/WavesurferAudioVisualizer';
 
 import './styles.css';
 
 function CallVisualizer() {
-	// let wavePlayer = new WavePlayer({
-	// 	container: '#waveform',
-	// 	barWidth: 4,
-	// 	barGap: 1,
-	// 	height: 128,
-	// });
-
-	// wavePlayer.load('url-to-some-audio-file.mp3').then(() => wavePlayer.play());
-
-	// wavePlayer.createPlaylist(['1.mp3', '2.mp3', '3.mp3', '4.mp3', '5.mp3']);
-	// wavePlayer.on('waveplayer:playlist:ready', (me) => {
-	// 	me.play();
-	// });
-
 	const { data } = useDemoData({
 		dataSet: 'Commodity',
 		rowLength: 100,
@@ -39,9 +24,7 @@ function CallVisualizer() {
 				</Card>
 			</div>
 			<div>
-				<Visualizer />
-				{/* <WavePlayer /> */}
-				{/* <AudioVisualizer /> */}
+				<WavesurferAudioVisualizer />
 			</div>
 			<div className='calls-visualizer-card-list'>
 				<div className='calls-visualizer-moments-card'>
