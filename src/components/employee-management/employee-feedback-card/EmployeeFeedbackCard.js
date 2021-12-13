a450c3ad94aa2b4d1330d976c01002c2e579ab55
Wasi <wasimuka@gmail.com>
2021-12-13 13:14:31 +0530

    worked for the feedback

--- src/components/employee-management/employee-feedback-card/EmployeeFeedbackCard.js
+++ src/components/employee-management/employee-feedback-card/EmployeeFeedbackCard.js
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { Card, Typography } from '@mui/material';
 
 import EmployeeManagementFeedback from '../employee-management-feedback/EmployeeManagementFeedback';
 
 import './styles.css';
+import { useSelector } from 'react-redux';
+import Axios from '../../../http/Axios';
 
 function EmployeeFeedbackCard() {
+	const {relodeFeedback, selectedUser} = useSelector(store => store.user);
+	const [feedbacks, setFeedback] = useState([]);
+	useEffect(async () =>{
+		if(selectedUser?.name){
+			(async ()=>{
+				const res = await Axios.post('/get-feedback', {recipient_id: selectedUser.name});
+				if(res.isSuccess){
+					setFeedback(res.data);
+				}
+			})();
+		}
+	}, [relodeFeedback, selectedUser])
 	return (
 		<div>
 			<div className='employee-feedback-card-title'>
 				<Typography variant='subtitle1'>Continuous Feedback:</Typography>
 			</div>
+			{feedbacks.map(feedback => <EmployeeManagementFeedback {...feedback} />)}
+			{/* <EmployeeManagementFeedback />
 			<EmployeeManagementFeedback />
-			<EmployeeManagementFeedback />
-			<EmployeeManagementFeedback />
+			<EmployeeManagementFeedback /> */}
 		</div>
 	);
 }
