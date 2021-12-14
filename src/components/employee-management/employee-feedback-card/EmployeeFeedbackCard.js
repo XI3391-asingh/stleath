c4738cf16c957d0d207d8bdefa24f769a6b8da53
Wasi <wasimuka@gmail.com>
2021-12-14 12:16:28 +0530

    new changes in reload feed back

--- src/components/employee-management/employee-feedback-card/EmployeeFeedbackCard.js
+++ src/components/employee-management/employee-feedback-card/EmployeeFeedbackCard.js
@@ -5,10 +5,12 @@ import { Card, Typography } from '@mui/material';
 import EmployeeManagementFeedback from '../employee-management-feedback/EmployeeManagementFeedback';
 
 import './styles.css';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Axios from '../../../http/Axios';
+import { SET_RELOAD_FEEDBACK } from '../../../store/type';
 
 function EmployeeFeedbackCard() {
+	const dispatch = useDispatch();
 	const { relodeFeedback, selectedUser } = useSelector((store) => store.user);
 	const [feedbacks, setFeedback] = useState([]);
 	useEffect(async () => {
@@ -18,6 +20,7 @@ function EmployeeFeedbackCard() {
 					recipient_id: selectedUser?.name || localStorage.getItem('username'),
 				});
 				if (res.isSuccess) {
+					dispatch({type: SET_RELOAD_FEEDBACK, payload: false});
 					setFeedback(res.data);
 				}
 			})();
